51dfd5d6e0261d8ff955bee82d6e96c208836268
杨勇 <1994101600@qq.com>
2022-10-25 21:06:03 +0800

    refactor: unlike site remover

--- README.md
+++ README.md
@@ -1,20 +1,9 @@
-<h2 align="center">Remove W3Schools</h2>
+<h2 align="center">Remove Unlike Website</h2>
 
-  <p align="center">
-    <a href="https://chrome.google.com/webstore/detail/remove-w3schools/gohnadkcefpdhblajddfnhapimpdjkje"><img src="https://img.shields.io/chrome-web-store/d/gohnadkcefpdhblajddfnhapimpdjkje.svg" alt="downloads"></a>
-  </p>
-  <p align="center"><a href="https://chrome.google.com/webstore/detail/remove-w3schools/gohnadkcefpdhblajddfnhapimpdjkje"><b>Install straight from the Chrome webstore</b></a></p>
-  <p align="center"><img src="https://github.com/GMaiolo/remove-w3schools/blob/master/images/icon128.png" alt="logo"></p>
+_Chrome plugin to remove Unlike Site results in google searches_
 
-_Chrome plugin to remove W3Schools results in google searches_
+Unlike Site List:
 
-+ **Feel free to open issues or send pull requests for improvements/suggestions/bugs.**
+1. w3cschool
+2. csdn
 
-Manual Installation: 
-
-1. Clone/download the project
-2. Go to [chrome extensions page](chrome://extensions/)
-3. Click on "Load unpacked extension..." button
-4. Select the project folder
-5. ??
-6. Profit!
--- scripts/remover.js
+++ scripts/remover.js
@@ -1,29 +1,29 @@
-(function() {
-    var W3SchoolsRemover = {
-        currentUrl: {}, 
+(function () {
+    var UnlikeSiteRemover = {
+        currentUrl: {},
         constants: {
             queries: {
-                result_links: '.g div > a[href*="www.w3schools.com"]', 
-                link_parent_node: '#rso div.g', 
+                result_links: ['.g div > a[href*="www.w3schools.com"]', '.g div > a[href*="blog.csdn.net"]'],
+                link_parent_node: '#rso div.g',
                 main_google_node: 'main'
-            }, 
+            },
             events: {
-                get_info: 'get_tId_and_wId', 
-                inactive: 'inactive', 
+                get_info: 'get_tId_and_wId',
+                inactive: 'inactive',
                 active: 'active'
-            }, 
+            },
             console: {
-                needs_to_be_updated: 'W3SchoolsRemover selectors need to be updated!', 
-                removed: 'W3Schools links were removed from this search.'
-            }, 
+                needs_to_be_updated: 'UnlikeSiteRemover selectors need to be updated!',
+                removed: 'UnlikeSiteRemover links were removed from this search.'
+            },
             observerConfig: { childList: true, subtree: true }
-        }, 
-        init: function() {
+        },
+        init: function () {
             var mainGoogleNode = document.getElementById(this.constants.queries.main_google_node);
             /* avoiding google new tab page and other variations */
-            if(!mainGoogleNode) {
+            if (!mainGoogleNode) {
                 return chrome.runtime.sendMessage({ event: this.constants.events.inactive, url: window.location.href });
-            } 
+            }
             chrome.runtime.sendMessage({ event: this.constants.events.get_info }, (info) => {
                 var tId = info.tId;
                 var wId = info.wId;
@@ -32,22 +32,26 @@
                 this.remove(info);
                 this.createResultsObserver(mainGoogleNode);
             });
-        }, 
-        getAllW3Links: function() {
-            return document.querySelectorAll(this.constants.queries.result_links);
-        }, 
-        remove: function(info) {
+        },
+        getAllUnlikeSiteLinks: function () {
+            const links = []
+            this.constants.queries.result_links.forEach(function (linkTag) {
+                links.push(...document.querySelectorAll(linkTag))
+            })
+            return links;
+        },
+        remove: function (info) {
             var tId = info.tId;
             var wId = info.wId;
             // ignoring dropdown items and huge card on the right
-            var links = Array.from(this.getAllW3Links()).filter(function (link) {
+            var links = Array.from(this.getAllUnlikeSiteLinks()).filter(function (link) {
                 var isAccordionItem = Boolean(link.closest('g-accordion-expander'))
                 var isHugeCardOnTheRight = Boolean(link.closest('#wp-tabs-container'))
                 return !isAccordionItem && !isHugeCardOnTheRight
             });
             var count = links.length;
-            if(!count) {
-                if(!this.isSameUrl(window.location.href, info)) {
+            if (!count) {
+                if (!this.isSameUrl(window.location.href, info)) {
                     chrome.runtime.sendMessage({ event: this.constants.events.inactive });
                     this.currentUrl[wId][tId] = window.location.href;
                 }
@@ -57,8 +61,8 @@
             chrome.runtime.sendMessage({ event: this.constants.events.active, count: count });
             console.info(count + ' ' + this.constants.console.removed);
             links.forEach(this.deleteOldGrandpaNode.bind(this));
-        }, 
-        createResultsObserver: function(mainGoogleNode) {
+        },
+        createResultsObserver: function (mainGoogleNode) {
             this.resultsObserver = new MutationObserver(() => {
                 chrome.runtime.sendMessage({ event: this.constants.events.get_info }, info => {
                     var tId = info.tId;
@@ -68,15 +72,15 @@
                 });
             });
             this.resultsObserver.observe(mainGoogleNode, this.constants.observerConfig);
-        }, 
-        isSameUrl: function(currentUrl, info) {
+        },
+        isSameUrl: function (currentUrl, info) {
             var tId = info.tId;
             var wId = info.wId;
             return this.currentUrl[wId][tId] === currentUrl;
-        }, 
-        deleteOldGrandpaNode: function(el) {
+        },
+        deleteOldGrandpaNode: function (el) {
             var parent = el.closest(this.constants.queries.link_parent_node);
-            if(!parent) return console.warn(this.constants.console.needs_to_be_updated);
+            if (!parent) return console.warn(this.constants.console.needs_to_be_updated);
             parent.style.display = 'none';
         }
     };
@@ -84,7 +88,7 @@
     /* may need to tune this timeout in the future
     otherwise we get progressive removals instead of all them toghether */
     setTimeout(() => {
-        W3SchoolsRemover.init();
+        UnlikeSiteRemover.init();
     }, 250)
 
 })();
